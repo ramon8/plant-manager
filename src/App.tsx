162c00e9e9d4bde86bc1fadc28700498abeb17a3
Ramón Fernández Imbernón <ramon.fer96@gmail.com>
2025-06-03 21:31:07 +0200

    fix add plant photo state (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './styles/GlobalStyles';
