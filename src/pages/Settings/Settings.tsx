ad6f43bcdc60253c77ed8a2d11f73b8c30d08eb6
Ramón Fernández Imbernón <ramon.fer96@gmail.com>
2025-06-03 22:05:21 +0200

    Add basic styles to settings placeholder

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import { SettingsContainer } from './Settings.styles';
 
 const Settings: React.FC = () => {
-    return <div>Settings</div>;
+    return (
+        <SettingsContainer>
+            <h1>Settings</h1>
+            <p>Manage application preferences here.</p>
+        </SettingsContainer>
+    );
 };
 
 export default Settings;
