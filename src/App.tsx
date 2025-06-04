5641f0f6be6a9a50c216ff89f34cf81e350fdaf6
Ramón Fernández Imbernón <ramon.fer96@gmail.com>
2025-06-04 18:42:59 +0200

    feat: add page transition animations (#10)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { CustomThemeProvider } from './theme/ThemeContext';
 import { AppContainer, MainContent } from './App.styles';
@@ -9,6 +10,36 @@ import AddPlant from './pages/Add';
 import PlantDetail from './pages/PlantDetail';
 import Settings from './pages/Settings';
 
+const pageVariants = {
+  initial: { x: '100%', opacity: 0 },
+  animate: { x: 0, opacity: 1 },
+  exit: { x: '-100%', opacity: 0 },
+};
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        variants={pageVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ duration: 0.3 }}
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/plants/:id" element={<PlantDetail />} />
+          <Route path="/add" element={<AddPlant />} />
+          <Route path="/care" element={<Care />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <CustomThemeProvider>
@@ -16,13 +47,7 @@ function App() {
       <Router>
         <AppContainer>
           <MainContent>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/plants/:id" element={<PlantDetail />} />
-              <Route path="/add" element={<AddPlant />} />
-              <Route path="/care" element={<Care />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
+            <AnimatedRoutes />
           </MainContent>
           <Navigation />
         </AppContainer>
